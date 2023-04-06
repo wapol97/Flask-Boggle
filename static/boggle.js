c7eae721b5bb2dea81a7ade34a4334aa23806fb1
Wade <wadeapollard@gmail.com>
2023-04-06 15:09:49 -0400

    #

--- static/boggle.js
+++ static/boggle.js
@@ -20,5 +20,62 @@ class BoggleGame {
         $(".score", this.board).text(this.score);
     }
 
-    
+    showMessage(msg, cls) {
+        $(".msg", this.board)
+        .text(msg)
+        .removeClass()
+        .addClass(`msg ${cls}`);
+    }
+
+    async handleSubmit(e) {
+        e.preventDefault();
+        const $word = $(".word", this.board)
+
+        let word = $word.val();
+        if(!word) return;
+
+        if(this.words.has(word)) {
+            this.showMessage(`Already found ${word}`, "err")
+            return;
+        }
+
+        const resp = await axios.get("/check-word", {params: {word: word}});
+        if(resp.data.result === "not-word") {
+            this.showMessage(`${word} is not a valid word`, "err");
+        } else if (resp.data.result === "not-on-board") {
+            this.showMessage(`${word} is not valid on this board`, "err");
+        } else {
+            this.showWord(word)
+            this.score += word.length;
+            this.showScore();
+            this.words.add(word);
+            this.showMessage(`Added: ${word}`, "ok");
+        }
+        $word.val("").focus();
+    }
+
+    showTimer() {
+        $(".timer", this.board).text(this.secs)
+    }
+
+    async Tick() {
+        this.secs = -1;
+        this.showTimer();
+
+        if(this.secs === 0) {
+            clearInterval(this.timer);
+            await this.scoreGame();
+        }
+    }
+
+    async scoreGame() {
+        $(".add-word", this.board).hide();
+        const resp = await axios.post("/post-score", {score: this.score});
+        if(resp.data.brokeRecord) {
+            this.showMessage(`New Record: ${this.score}`, "OK");
+        } else {
+            this.showMessage(`Final score: ${this.score}`, "OK");
+        }
+    }
+
 }
